1ec0169e34b29049ce6be290a729fead4a5dceab
caiopo <caiopoliveira@gmail.com>
2016-06-14 09:47:40 -0300

    General improvements

--- src/handler.py
+++ src/handler.py
@@ -4,10 +4,13 @@ import time
 import responses
 import requests
 import json
+from os.path import dirname, realpath
 
 JSON_SOURCE = 'https://voucomerno.ru/menu.json'
+AUTOMSG_PATH = dirname(realpath(__file__)) + '/automsg.txt'
 
 # Botfather: /setcommands
+# quibe - cardápio do RU
 # help - sobre quibebot
 
 def start(bot, update):
@@ -28,22 +31,61 @@ def quibe(bot, update):
 	try:
 		resp = requests.get(JSON_SOURCE)
 
-		cardapio = json.loads(resp.text)
-
-		resposta = ('hoje tem\n'
-					'{main}\n'
-					'{complement} pra complementar\n'
-					'vai ter {salad} de salada\n'
-					'e de sobremesa tem {dessert}').format(
-					main=cardapio['main'],
-					complement=cardapio['complement'],
-					salad=cardapio['salad'],
-					dessert=cardapio['dessert'])
-
 		bot.sendMessage(chat_id=update.message.chat_id,
-						text=resposta)
+						text=responses.cardapio(resp.json()))
 	except Exception as e:
 		_something_wrong(bot, update, e)
+
+def subscribe(bot, update):
+	global automsg_targets
+
+	if update.message.chat_id not in automsg_targets:
+		automsg_targets.append(update.message.chat_id)
+		bot.sendMessage(chat_id=update.message.chat_id,
+						text=responses.subscribe)
+	else:
+		bot.sendMessage(chat_id=update.message.chat_id,
+						text=responses.already_subscribed)
+	_save_automsg()
+
+
+def unsubscribe(bot, update):
+	global automsg_targets
+
+	if update.message.chat_id in automsg_targets:
+		automsg_targets.remove(update.message.chat_id)
+
+		bot.sendMessage(chat_id=update.message.chat_id,
+						text=responses.unsubscribe)
+	else:
+		bot.sendMessage(chat_id=update.message.chat_id,
+						text=responses.not_subscribed)
+
+
+def auto_msg_job(bot):
+	timenow = time.localtime()
+
+	# sum 2 to correct the time difference between the cloud server and the real UTC-3
+	if (timenow.tm_hour + 2) in AUTO_MSG_TIME and timenow.tm_min == 0:
+		for chat_id in auto_msg_targets:
+			try:
+
+			except Exception as e:
+				traceback.print_exc()
+
+def _start_automsg():
+	global automsg_targets
+	try:
+		with open('automsg.txt') as targets:
+			automsg_targets = [int(chat_id.strip('\n')) for chat_id in targets]
+	except FileNotFoundError:
+		open('automsg.txt', 'w').close()
+		automsg_targets = []
+
+def _save_automsg():
+	with open('automsg.txt', 'w') as targets:
+		for chat_id in auto_msg_targets:
+			targets.write(str(chat_id)+'\n')
 
 def _something_wrong(bot, update, e):
 	bot.sendMessage(chat_id=update.message.chat_id,
--- src/responses.py
+++ src/responses.py
@@ -6,4 +6,36 @@ Inspirado em https://voucomerno.ru/ (também é de onde eu pego o cardápio)
 
 Meu repositório: http://github.com/caiopo/quibe-bot"""
 
+_cardapio = """hoje tem:
+{main}
+{complement}
+{salad}
+{dessert}"""
+
 unknown_command = 'Que isso?'
+
+subscribe = 'Inscrito!'
+
+already_subscribed = 'Você já está inscrito!'
+
+unsubscribe = 'Inscricão removida!'
+
+not_subscribed = 'Você não está inscrito!'
+
+def cardapio(menu):
+	return _cardapio.format(
+			main=menu['main'],
+			complement=menu['complement'],
+			salad=menu['salad'],
+			dessert=menu['dessert'])
+
+if __name__ == '__main__':
+	import json
+	import requests
+	from handler import JSON_SOURCE
+
+	resp = requests.get(JSON_SOURCE)
+
+	menu = json.loads(resp.text)
+
+	print(cardapio(menu))
