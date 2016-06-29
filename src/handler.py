2ebb6b20a16d3d601e3c9cf711d4f9e223540ccd
caiopo <caiopoliveira@gmail.com>
2016-06-29 16:20:30 -0300

    Fix subscribing error

--- src/handler.py
+++ src/handler.py
@@ -20,29 +20,42 @@ AUTO_MSG_TIME = (10, 40)
 
 recently_sent = False
 
+def report_errors(func):
+	def catcher(bot, update):
+		try:
+			func(bot, update)
+		except Exception as e:
+			error(bot, update, e)
+	return catcher
+
+
+@report_errors
 def start(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id,
 					text=responses.start)
 
-
+@report_errors
 def help(bot, update):
 	bot.sendMessage(chat_id=update.message.chat_id,
 					text=responses.help,
 					disable_web_page_preview=True)
 
+@report_errors
 def unknown(bot, update):
 	if update.message.chat_id > 0: # user
 		bot.sendMessage(chat_id=update.message.chat_id,
 						text=responses.unknown_command)
 
+@report_errors
 def quibe(bot, update):
 	_send_menu(bot, update.message.chat_id)
 
+@report_errors
 def subscribe(bot, update):
 	global automsg_targets
 
-	if update.message.chat_id not in automsg_targets:
-		automsg_targets.append(update.message.chat_id)
+	if str(update.message.chat_id) not in automsg_targets:
+		automsg_targets.append(str(update.message.chat_id))
 
 		bot.sendMessage(chat_id=update.message.chat_id,
 						text=responses.subscribe)
@@ -51,11 +64,12 @@ def subscribe(bot, update):
 		bot.sendMessage(chat_id=update.message.chat_id,
 						text=responses.already_subscribed)
 
+@report_errors
 def unsubscribe(bot, update):
 	global automsg_targets
 
-	if update.message.chat_id in automsg_targets:
-		automsg_targets.remove(update.message.chat_id)
+	if str(update.message.chat_id) in automsg_targets:
+		automsg_targets.remove(str(update.message.chat_id))
 
 		bot.sendMessage(chat_id=update.message.chat_id,
 						text=responses.unsubscribe)
@@ -64,6 +78,7 @@ def unsubscribe(bot, update):
 		bot.sendMessage(chat_id=update.message.chat_id,
 						text=responses.not_subscribed)
 
+@report_errors
 def sendto(bot, update):
 	if str(update.message.chat_id) != config.MAINTAINER_ID:
 		bot.sendMessage(chat_id=update.message.chat_id,
@@ -86,19 +101,22 @@ def error(bot, update, e):
 					text='Error on @quibebot\nUpdate: {}\nError type: {}\nError: {}'.format(update, type(e), e))
 
 def auto_msg_job(bot):
-	global automsg_targets, recently_sent
+	try:
+		global automsg_targets, recently_sent
 
-	if recently_sent:
-		recently_sent = False
-		return
+		if recently_sent:
+			recently_sent = False
+			return
 
-	if _valid_time():
-		menu_dict = requests.get(JSON_SOURCE).json()
+		if _valid_time():
+			menu_dict = requests.get(JSON_SOURCE).json()
 
-		recently_sent = True
+			recently_sent = True
 
-		for chat_id in automsg_targets:
-			_send_menu(bot, chat_id, menu_dict)
+			for chat_id in automsg_targets:
+				_send_menu(bot, chat_id, menu_dict)
+	except Exception as e:
+		error(bot, None, e)
 
 def _send_menu(bot, chat_id, menu_dict=None):
 	if not menu_dict:
@@ -125,10 +143,16 @@ def _start_automsg():
 		open(AUTO_MSG_PATH, 'w').close()
 		automsg_targets = []
 
+	print(automsg_targets)
+
 def _save_automsg():
+	global automsg_targets
 	with open(AUTO_MSG_PATH, 'w') as targets:
 		for chat_id in automsg_targets:
 			targets.write(chat_id+'\n')
+
+	print(automsg_targets)
+
 
 def _valid_time():
 	timetuple = datetime.now(timezone('America/Sao_Paulo')).timetuple()
